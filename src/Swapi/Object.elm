0f4cef5fbf3f68d13a4fe6e46cfe4ee5c13abf74
Dillon Kearns <dillon@dillonkearns.com>
2017-12-12 23:11:09 -0800

    Expose Object and add docs.

--- src/Swapi/Object.elm
+++ src/Swapi/Object.elm
@@ -1,13 +1,24 @@
 module Swapi.Object exposing (..)
 
+{-| Type declarations used to give you type-safety so you only access types
+from the appropriate object.
+@docs Character, Droid, Human
+-}
 
+
+{-| Character
+-}
 type Character
     = Character
 
 
+{-| Droid
+-}
 type Droid
     = Droid
 
 
+{-| Human
+-}
 type Human
     = Human
