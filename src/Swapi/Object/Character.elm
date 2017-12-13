add410d610f72dc083b17c049f82f39a44574ba1
Dillon Kearns <dillon@dillonkearns.com>
2017-12-12 22:47:13 -0800

    Add character docs.

--- src/Swapi/Object/Character.elm
+++ src/Swapi/Object/Character.elm
@@ -1,35 +1,47 @@
 module Swapi.Object.Character exposing (..)
 
-import Swapi.Enum.Episode
-import Swapi.Object
-import Graphqelm.Builder.Argument as Argument exposing (Argument)
+{-| Code for retrieving fields from a Character in a type-safe way.
+@docs build, appearsIn, friends, id, name
+-}
+
 import Graphqelm.Builder.Object as Object
-import Graphqelm.Encode as Encode exposing (Value)
 import Graphqelm.FieldDecoder as FieldDecoder exposing (FieldDecoder)
 import Graphqelm.Object exposing (Object)
 import Json.Decode as Decode
+import Swapi.Enum.Episode
+import Swapi.Object
 
 
+{-| Specify the fields to use for the object.
+-}
 build : (a -> constructor) -> Object (a -> constructor) Swapi.Object.Character
 build constructor =
     Object.object constructor
 
 
+{-| Episodes the Character appears in.
+-}
 appearsIn : FieldDecoder (List Swapi.Enum.Episode.Episode) Swapi.Object.Character
 appearsIn =
     Object.fieldDecoder "appearsIn" [] (Swapi.Enum.Episode.decoder |> Decode.list)
 
 
+{-| Character's friends.
+-}
 friends : Object friends Swapi.Object.Character -> FieldDecoder (List friends) Swapi.Object.Character
 friends object =
     Object.listOf "friends" [] object
 
 
+{-| Character's id.
+-}
 id : FieldDecoder String Swapi.Object.Character
 id =
     Object.fieldDecoder "id" [] Decode.string
 
 
+{-| Character's name.
+-}
 name : FieldDecoder String Swapi.Object.Character
 name =
     Object.fieldDecoder "name" [] Decode.string
