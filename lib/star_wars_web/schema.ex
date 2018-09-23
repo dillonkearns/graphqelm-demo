8a3ffd17a968d3d46a9b42afb7b7ce862062901e
Dillon Kearns <dillon@dillonkearns.com>
2018-09-23 11:12:49 -0700

    Point to urls for offline viewing.

--- lib/star_wars_web/schema.ex
+++ lib/star_wars_web/schema.ex
@@ -6,15 +6,14 @@ defmodule StarWarsWeb.Schema do
   @luke %Character{
     id: "1000",
     name: "Luke Skywalker",
-    avatar_url:
-      "http://www.diszine.com/wp-content/uploads/2013/02/luke-skywalker-mark-hamill.jpg",
+    avatar_url: "/avatars/luke.jpg",
     friends: ["1002", "1003", "2000", "2001"],
     home_planet: "Tatooine"
   }
   @vader %Character{
     id: "1001",
     name: "Darth Vader",
-    avatar_url: "https://avatarfiles.alphacoders.com/468/46847.jpg",
+    avatar_url: "/avatars/vader.jpg",
     friends: ["1004"],
     home_planet: "Tatooine"
   }
@@ -22,15 +21,14 @@ defmodule StarWarsWeb.Schema do
   @han %Character{
     id: "1002",
     name: "Han Solo",
-    avatar_url:
-      "http://comic-cons.xyz/wp-content/uploads/Star-Wars-avatars-Movie-Han-Solo-Harrison-Ford.jpg",
+    avatar_url: "avatars/solo.jpg",
     friends: ["1000", "1003", "2001"]
   }
 
   @leia %Character{
     id: "1003",
     name: "Leia Organa",
-    avatar_url: "https://78.media.tumblr.com/avatar_60b17e7d34ad_128.pnj",
+    avatar_url: "/avatars/leia.pnj",
     friends: ["1000", "1002", "2000", "2001"],
     home_planet: "Alderaan"
   }
@@ -38,23 +36,24 @@ defmodule StarWarsWeb.Schema do
   @tarkin %Character{
     id: "1004",
     name: "Wilhuff Tarkin",
-    avatar_url:
-      "https://timedotcom.files.wordpress.com/2016/12/grand-moff-tarkin1.jpg?quality=30",
+    avatar_url: "/avatars/tarkin.jpg",
     friends: ["1001"]
   }
 
   @threepio %Character{
     id: "2000",
     name: "C-3PO",
-    avatar_url: "https://pbs.twimg.com/profile_images/22039052/03.01.c3po_400x400.jpg",
-    friends: ["1000", "1002", "1003", "2001"]
+    avatar_url: "/avatars/3po.jpg",
+    friends: ["1000", "1002", "1003", "2001"],
+    home_planet: "Tatooine"
   }
 
   @artoo %Character{
     id: "2001",
     name: "R2-D2",
-    avatar_url: "https://giffiles.alphacoders.com/884/thumb-8849.jpg",
-    friends: ["1000", "1002", "1003"]
+    avatar_url: "/avatars/r2.jpg",
+    friends: ["1000", "1002", "1003"],
+    home_planet: "Tatooine"
   }
 
   use Absinthe.Schema
@@ -65,8 +64,8 @@ defmodule StarWarsWeb.Schema do
     field(:id, non_null(:id))
     @desc "The name of the character."
     field(:name, non_null(:string))
-    # @desc "Url to a profile picture for the character."
-    # field(:avatar_url, type: non_null(:string))
+    @desc "Url to a profile picture for the character."
+    field(:avatar_url, type: non_null(:string))
     @desc "The friends of the character, or an empty list if they have none."
     field :friends, type: non_null(list_of(non_null(:character))) do
       resolve(fn character, _, _ ->
@@ -91,12 +90,12 @@ defmodule StarWarsWeb.Schema do
   end
 
   query do
-    # @desc "Get all known characters."
-    # field :all, type: non_null(list_of(non_null(:character))) do
-    #   resolve(fn _, _, _ ->
-    #     {:ok, [@luke, @leia, @han, @vader, @threepio, @artoo]}
-    #   end)
-    # end
+    @desc "Get all known characters."
+    field :all, type: non_null(list_of(non_null(:character))) do
+      resolve(fn _, _, _ ->
+        {:ok, [@luke, @leia, @han, @vader, @tarkin, @threepio, @artoo]}
+      end)
+    end
 
     field :character, :character do
       @desc "ID of the character."
